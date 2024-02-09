b2d71d0b45103a37f3cfb8e2248b99b4cf1b3678
Moreno La Quatra <moreno.laquatra@gmail.com>
2024-02-09 08:46:42 +0100

    Added emotions

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # Sanremo 2024
 
-Una semplice wordcloud per le canzoni in gara alla 74esima edizione del Festival di Sanremo.
+Una semplice analisi delle le canzoni in gara alla 74esima edizione del Festival di Sanremo.
 
 ## Wordcloud
 
 ![Wordcloud](wordcloud-sanremo-24.png)
+
+## Emotion distribution
+
+![Emotion distribution](emotion-distribution-sanremo-24.png)
+
+Per trovare le emozioni è stato utilizzato il modello [`MilaNLProc/feel-it-italian-emotion`](https://huggingface.co/MilaNLProc/feel-it-italian-emotion). Una descrizione completa [qui](https://aclanthology.org/2021.wassa-1.8/). Un ringraziamento particolare a [MilaNLProc](https://huggingface.co/MilaNLProc).
 
 ## How to?
 
---
+++ emotion.py
@@ -0,0 +1,64 @@
+from transformers import pipeline, AutoTokenizer
+import os, glob
+
+model_tag = "MilaNLProc/feel-it-italian-emotion"
+
+text_classifier = pipeline("text-classification", model=model_tag, tokenizer=model_tag)
+tokenizer = AutoTokenizer.from_pretrained(model_tag)
+
+files = glob.glob("songs_text/*.txt")
+song_names = []
+labels = []
+for file in files:
+    f = open(file, "r")
+    text = f.read().replace("’", "'").replace("\n", " ")
+    # take the first 512 tokens
+    tokens = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=256)
+    # decode the tokens 
+    text = tokenizer.decode(tokens["input_ids"][0])
+    f.close()
+    c = text_classifier(text)
+    labels.append(c[0]["label"])
+    song_name = os.path.basename(file).replace(".txt", "")
+    song_name = song_name.replace("_", " ")
+    song_name = song_name.replace("-", " - ")
+    # capitalize the first letter of each word
+    song_name = " ".join([word.capitalize() for word in song_name.split()])
+    song_names.append(song_name)
+    
+# print the results
+for i in range(len(song_names)):
+    print(f"{labels[i]}: {song_names[i]}")
+    
+# save as TSV file
+with open("emotion-distribution-sanremo-24.tsv", "w") as f:
+    f.write("song\temotion\n")
+    for i in range(len(song_names)):
+        f.write(f"{song_names[i]}\t{labels[i]}\n")
+    
+    
+import matplotlib.pyplot as plt
+
+# Define colors for each emotion label
+emotion_colors = {
+    "anger": "red",
+    "fear": "orange",
+    "joy": "green",
+    "sadness": "blue",
+}
+
+# Plotting
+emotion_counts = {label: labels.count(label) for label in set(labels)}
+
+# Plot the bar graph
+plt.figure(figsize=(10, 6))
+colors = ['blue', 'orange', 'green', 'red', 'purple']
+plt.bar(emotion_counts.keys(), emotion_counts.values(), color=colors)
+plt.xlabel('Emotion')
+plt.ylabel('Frequency')
+plt.title('Emotion Distribution in Songs')
+plt.xticks(rotation=45)
+plt.tight_layout()
+
+# Show plot
+plt.savefig("emotion-distribution-sanremo-24.png")
